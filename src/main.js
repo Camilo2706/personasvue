ef2a777b23cab7f6f45e7972fc4d36f9ab1b1c32
Camilo Hoyos <camilohoyos2706@gmail.com>
2024-11-18 22:29:47 -0500

    VUE REALIZADO

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
 import { createApp } from 'vue'
+
 import App from './App.vue'
 import router from './router'
 
---
+++ src/views/comunas.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Comunas</h1>
+    <ul>
+      <li v-for="comuna in comunas" :key="comuna.id">
+        {{ comuna.nombre }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      comunas: []
+    };
+  },
+  mounted() {
+    axios.get('http://localhost:8000/api/comunas')
+      .then(response => {
+        this.comunas = response.data;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+};
+</script>
